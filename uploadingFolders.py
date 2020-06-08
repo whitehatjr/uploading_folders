803d9506f9a0246545576a5e81e573c9aaedc02d
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2020-06-08 15:04:54 +0530

    solution 101

---
+++ uploadingFolders.py
@@ -0,0 +1,37 @@
+import os
+import dropbox
+from dropbox.files import WriteMode
+#
+class TransferData:
+    def __init__(self,access_token):
+        self.access_token =  access_token
+#
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+            # enumerate local files recursively
+        for root, dirs, files in os.walk(file_from):
+
+            for filename in files:
+                    # construct the full local path
+                local_path = os.path.join(root, filename)
+
+                    # construct the full Dropbox path
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                    # upload the file
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+
+def main():
+    access_token = 'riFu6Ybhc9AAAAAAAAAAHWkfE9AiGyD6n4254tOxesw7ShRjGjFhrjhRVa3NX3mx'
+    transferData = TransferData(access_token)
+
+    file_from = str(input("Enter the file path to transfer : -"))
+    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+
+    # API v2
+    transferData.upload_file(file_from,file_to)
+    print("file has been moved !!!")
+
+main()
